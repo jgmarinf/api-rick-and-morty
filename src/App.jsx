039c2a3016a8f84fe7d438ea8fa15c28e9a18f8e
Juan Guillermo <jgmarinf93@gmail.com>
2022-11-23 14:09:52 -0500

    subiendo el deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import CardCharacter from './components/CardCharacter'
 import InputSearch from './components/InputSearch'
 import LocationInfo from './components/LocationInfo'
+import Pagination from './components/Pagination'
 import useLocationApi from './hooks/useLocationApi';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   const {location} = useLocationApi(locationSearch)
   
   let arrayResidents = []
-  const residentsPerPage = 4
+  const residentsPerPage = 6
   if (location?.residents.length < residentsPerPage){
     arrayResidents = [...location?.residents]
   } else {
@@ -22,11 +23,26 @@ function App() {
   }
 
   let arrayPages =  []
+  let quantityPages = Math.ceil(location?.residents.length / residentsPerPage)
+  const pagesPerBlock = 5
+  let currentBlock = Math.ceil(currentPage / pagesPerBlock)
+  if(currentBlock * pagesPerBlock >= quantityPages){
+    for (let i = currentBlock * pagesPerBlock - pagesPerBlock + 1; i <= quantityPages ; i++) {
+      arrayPages.push(i)
+    }
+  } else {
+    for (let i = currentBlock * pagesPerBlock - pagesPerBlock + 1; i <= currentBlock * pagesPerBlock; i++) {
+      arrayPages.push(i)
+    }
+  }
+  
+ 
   return (
     <div className="App">
       <img className='imagen' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/1200px-Rick_and_Morty.svg.png' ></img>
-      <InputSearch setLocationSearch={setLocationSearch} />
+      <InputSearch setLocationSearch={setLocationSearch} />      
       <LocationInfo location ={location} />
+      <Pagination arrayPages = {arrayPages} currentPage= {currentPage} setCurrentPage= {setCurrentPage} quantityPages= {quantityPages}/>
       <div className='content_cards'>
         {
           arrayResidents?.map(resident=> (
@@ -34,6 +50,7 @@ function App() {
           ))
         }
       </div>
+      <Pagination arrayPages = {arrayPages} currentPage= {currentPage} setCurrentPage= {setCurrentPage} quantityPages= {quantityPages}/>
     </div>
   )
 }
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useRef } from 'react'
+
+const Pagination = ({arrayPages, currentPage, setCurrentPage, quantityPages}) => {
+
+    const listNumber = useRef()
+
+
+    const prevPage = () => {
+        if(currentPage - 1 === 0) {
+            setCurrentPage(quantityPages)
+        } else{
+            setCurrentPage(currentPage-1)
+        }
+    }
+
+    const nextPage = () => {
+        if(currentPage + 1 === quantityPages) {
+            setCurrentPage(1)
+        } else{
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+    const changePageTo = n => setCurrentPage(n)
+    
+  return (
+    <div className='pagination-container'>
+        <div onClick={prevPage} className='pagination-prev-netx'>&#60;</div>
+        <ul ref={listNumber} className='pagination-number-container'>
+            {
+                arrayPages?.map(num => (
+                    <li 
+                        onClick={() => changePageTo(num)} 
+                        key={num} 
+                        className={currentPage === num ? `page-number page-active` : `page-number` }
+                    >{num}</li>
+                ))
+            }
+        </ul>
+        <div onClick={nextPage} className='pagination-prev-netx'>&#62;</div>
+    </div>
+  )
+}
+
+export default Pagination
